77ea9a567c956d693e4b68f1447c761468a133d1
Eduardo Marín <emarin.tech@gmail.com>
2025-08-02 21:13:53 +0100

    sabado

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -74,4 +74,4 @@ python manage.py collectstatic --noinput
 log "Starting Gunicorn..."
 exec gunicorn Corpus2026.wsgi:application --bind 0.0.0.0:${PORT:-8000} --workers 2 --threads 4 --timeout 0 --access-logfile - --error-logfile - --capture-output --enable-stdio-inheritance --log-level debug
 
-sleep infinity
+
